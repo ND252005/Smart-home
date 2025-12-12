6b902169f79bbdccfb77b8b4f4c45a2e00550298
ND250205 <nguyenduc250205@gmail.com>
2025-12-12 11:16:52 +0700

    update sensor

--- REPORT_MIDDLE_PROJECT/libraries/config/config.cpp
+++ REPORT_MIDDLE_PROJECT/libraries/config/config.cpp
@@ -1,5 +1,5 @@
 #include "config.h"
 
-String SERVER_URL = "http://192.168.31.232:8080";
+String SERVER_URL = "http://192.168.56.1:8080";
 // String SERVER_URL = "http://192.168.31.136:8000";
 String HASHCODE = "f7b11509f4d6";
